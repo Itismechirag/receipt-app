3fffa92ac4f5d43c3c50e697be512a2e7918b65b
Chirag <chiragupta2005@gmail.com>
2026-01-22 00:28:49 +0530

    install fearure added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'billtemplate.png'], // Add your background image here
+      manifest: {
+        name: 'Jeevan Jyoti Receipt App',
+        short_name: 'JJ Receipts',
+        description: 'Standalone Billing App for Hospital',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png', // You can use online generators to make these
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+});
